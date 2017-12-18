8832fc60a1c99cc96d09eed53cd10453983379b8
EspiDev <aguy867@gmail.com>
2017-12-18 10:09:49 -0500

    poj

--- solutions/c++/poj/poj1442.cpp
+++ solutions/c++/poj/poj1442.cpp
@@ -8,30 +8,20 @@ int M, N;
 
 /*
  * POJ 1442
+ * Utilizes 2 priority queues.
  */
 
 int main() {
     cin >> M >> N;
     int incur[M];
-    for(int i = 0; i < M; i++) {
-        cin >> incur[i];
-    }
+    for(int i = 0; i < M; i++) cin >> incur[i];
     priority_queue<int> ass;
     priority_queue<int, vector<int>, greater<int> > ass2;
-    int cur = 0, d = 0;
-    vector<int> v;
+    int cur = 0, d = 0, first = 0;
     for(int i = 0; i < N; i++) {
         int c;
         cin >> c;
-        v.push_back(c);
-    }
-    sort(v.begin(), v.end());
-
-    int first = 0;
-
-    for(int i = 0; i < N; i++) {
-        int c = v[i];
-        for(; cur < c; cur++) {
+        while(cur < c) {
             if(i == 0 || incur[cur] >= first) {
                 ass2.push(incur[cur]);
             }
@@ -40,13 +30,12 @@ int main() {
                 ass2.push(ass.top());
                 ass.pop();
             }
+            cur++;
         }
         first = ass2.top();
-
         cout << ass2.top() << endl;
         ass.push(ass2.top());
         ass2.pop();
-
         d++;
     }
     return 0;
