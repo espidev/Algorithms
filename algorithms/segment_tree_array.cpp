58f42370483d5a869f8a9f1ac425ca349234e06b
EspiDev <espidev@gmail.com>
2018-07-28 15:51:56 -0400

    fixed segment_tree

--- algorithms/segment_tree_array.cpp
+++ algorithms/segment_tree_array.cpp
@@ -15,61 +15,89 @@ int *arr, *input;
 
 // recursively fill tree bottom - up with values from input array
 
-void create_tree(int index, int begin_index , int end_index) {
-	if (end_index - begin_index < 2) { // reached bottom of tree, fill in with input value
-		arr[index] = input[begin_index];
+/*
+ * i = arr[] index
+ * bi = begin index range currently
+ * ei = end index range currently
+ */ 
+
+void create_tree(int i, int bi , int ei) { // caveat: the index of the tree starts at 1
+	if (ei == bi) { // reached bottom of tree, fill in with input value
+		arr[i] = input[bi];
 		return;
 	}
-	int mid = (begin_index + end_index) / 2;
-	create_tree(2 * index + 1, begin_index, mid);
-	create_tree(2 * index + 2, mid, end_index);
-	arr[index] = arr[index * 2 + 1] + arr[index * 2 + 2];
+	int mid = (bi + ei) / 2;
+	create_tree(2 * i, bi, mid);
+	create_tree(2 * i + 1, mid+1, ei);
+	arr[i] = arr[i * 2] + arr[i * 2 + 1];
 }
 
 // recursively go down binary tree and add together the stored sums
 
-int find_sum_for_range_util(int b_index, int e_index, int query_b, int query_e, int index) {
-	if (query_b <= b_index && e_index <= query_e) //if the segment is completely inside the required query
-		return arr[index];
+/*
+ * bi = begin index (left)
+ * ei = end index (right)
+ * qb = query begin
+ * qe = query end
+ * i = arr[] index
+ */
 
-	if (e_index < query_b || query_e < b_index) // if the segment completely outside of the required query
-		return 0;
-
-	int mid = (b_index + e_index) /2;
-	return find_sum_for_range_util(b_index, mid, query_b, query_e, 2 * index + 1) + find_sum_for_range_util(mid+1, e_index, query_b, query_e, 2 * index + 2);
+int query_u(int bi, int ei, int qb, int qe, int i) {
+	if (qb <= bi && ei <= qe) return arr[i]; //if the segment is completely inside the required query, return it
+	if (ei < qb || qe < bi) return 0; // if the segment completely outside of the required query, ignore it
+	int mid = (bi + ei) / 2; 
+	return query_u(bi, mid, qb, qe, 2 * i) + query_u(mid+1, ei, qb, qe, 2 * i + 1); // if part of the segment is in the query, go down the tree
 }
 
 // utility to find the sum for the range of indexes
 
-int find_sum_for_range(int b, int e) {
-	return find_sum_for_range_util(0, N-1, b, e, 0);
+/*
+ * b = beginning index to query
+ * e = end index to query (including itself)
+ */
+
+int query_range(int b, int e) {
+	return query_u(0, N-1, b, e, 1);
 }
 
 // recursively go down tree checking if the index is
 
-void update_value_util(int diff, int u_index, int b_index, int e_index, int index) {
-	if (u_index < b_index || u_index > e_index) return; // if the segment does not contain node return
-	arr[index] -= diff; // update value
+/*
+ * diff = difference to subtract by
+ * ui = index to update
+ * bi = begin index (left)
+ * ei = end index (right)
+ * i = arr[] index
+ */
 
-	if (b_index != e_index) {
-		int mid = (b_index + e_index) / 2;
-		update_value_util(diff, u_index, b_index, mid, 2 * index + 1);
-		update_value_util(diff, u_index, mid + 1, e_index, 2 * index + 2);
-	}
+void update_u(int diff, int ui, int bi, int ei, int i) {
+	arr[i] -= diff; // update value with new difference
+	if (bi == ei) return; // if the segment does not contain node return
+	
+	int mid = (bi+ei)/2;
+	if (ui > mid) update_u(diff, ui, mid+1, ei, i*2+1);
+	else update_u(diff, ui, bi, mid, i*2);
 }
 
-// update value at input index with new value
+// utility to update value at input index with new value
+
+/*
+ * index = index of original array to update
+ * val = value to update it to
+ */
 
 void update_value(int index, int val) {
 	int diff = input[index] - val;
-	update_value_util(diff, index, 0, N-1, 0);
+	update_u(diff, index-1, 1, N-1, 1);
+	input[index] -= diff;
 }
+
 
 int main() {
 	// example usage
 
 	N = 8; // size of array
-	arr = new int[2 * N - 1];
+	arr = new int[2 * N]; // ignore
 	input = new int[N];
 	
 	// populate input array with values	
@@ -83,17 +111,17 @@ int main() {
 	input[6] = 2;
 	input[7] = 6;
 
-	create_tree(0, 0, N); // fill segment tree with the input
+	create_tree(1, 0, N-1); // fill segment tree with the input
 
-	for (int i = 0; i < 2 * N - 1; i++) printf("%d\n", arr[i]);
+	for (int i = 1; i < 2 * N; i++) printf("%d\n", arr[i]);
 
-	printf("0 - 4: %d\n", find_sum_for_range(0, 4)); //find the sum of the range of values from 0 - 4
+	printf("0 - 4: %d\n", query_range(0, 4)); //find the sum of the range of values from 0 - 4
 
 	printf("Update index 0 from 5 to 10.\n");
 
 	update_value(0, 10); // updated index 0 to be 10
 
-	for (int i = 0; i < 2 * N - 1; i++) printf("%d\n", arr[i]);
+	for (int i = 1; i < 2 * N - 1; i++) printf("%d\n", arr[i]);
 
 	return 0;
 }
