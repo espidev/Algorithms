3896c9e9edc4e0b7f134a1ce5ebd14e6a331d9bf
EspiDev <espidev@gmail.com>
2018-12-16 11:34:36 -0500

    polish

--- algorithms/dijkstra.cpp
+++ algorithms/dijkstra.cpp
@@ -3,28 +3,31 @@ using namespace std;
 
 int N, M, S, D; // N = # of nodes, M = edges, S = start, D = destination
 
+// map: (node a, (node b, weight))
 int dijkstra(unordered_map<int, vector<pair<int, int>>>& graph, int start, int dest) {
-    int dist[N+1];
-    memset(dist, 1e9, sizeof dist);
-    dist[start] = 0;
+    // init
+    int weight[N+1];
+    memset(weight, 1e9, sizeof weight);
+    weight[start] = 0;
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; // weight, node
     pq.push(make_pair(0, start));
 
+    // bfs
     while (!pq.empty()) {
         pair<int, int> pv = pq.top();
         pq.pop();
+
         printf("%d %d\n", pv.first, pv.second);
-        if (pv.second == dest) break;
-        vector<pair<int, int>> v = graph.find(pv.second)->second;
-        for (pair<int, int> p : v) {
-            if (dist[p.first] > pv.second + p.second) {
-                dist[p.first] = pv.second + p.second;
-                pq.push(make_pair(dist[p.first], p.first));
+        for (pair<int, int> p : graph.find(pv.second)->second) { // node b, weight
+            if (weight[p.first] > pv.first + p.second) {
+                weight[p.first] = pv.first + p.second;
+                pq.push(make_pair(weight[p.first], p.first));
             }
         }
     }
-    return dist[dest];
+
+    return weight[dest];
 }
 
 int main() {
@@ -37,10 +40,10 @@ int main() {
         auto d = graph.find(a);
         if (d == graph.end()) {
             vector<pair<int, int>> v;
-            v.push_back(make_pair(b, w));
+            v.emplace_back(b, w);
             graph.insert(make_pair(a, v));
         } else {
-            d->second.push_back(make_pair(b, w));
+            d->second.emplace_back(b, w);
         }
     }
     printf("%d\n", dijkstra(graph, S, D));
