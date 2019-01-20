185fc34c0db88891d3af9a0f8be7cc8581bef3b9
EspiDev <espidev@gmail.com>
2019-01-19 22:50:33 -0500

    CCC18S4

--- solutions/ccc/ccc18s4.cpp
+++ solutions/ccc/ccc18s4.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 #define ull unsigned long long
-int N, arr[1000001];
-// todo iterate up to sqrt(N);
+#define MAXN 10000001
+ull N, arr[MAXN];
+
 ull rec(ull k) {
-	if (arr[k]) return arr[k];
-	if (k == 1) return 1;
-	ull ans = 0;
-	for (ull i = 2; k/i <= sqrt(k); i++) {
-		ans += rec(k/i);
-	}
-	ans += (k-sqrt(k))*rec(k-1);
-	arr[k] = ans;
-	return ans;
+    if (k < MAXN && arr[k]) return arr[k];
+    if (k == 1) return 1;
+    ull sum = 0;
+    for (ull i = 1; i <= k / 2;) {
+        ull d = k / i, d2 = k / (i + 1);
+        sum += (d - d2) * rec(i);
+        i = k / d2;
+    }
+    if (k < MAXN) arr[k] = sum;
+    return sum;
 }
 
 int main() {
-	cin >> N;
-	cout << rec(N) << endl;
-	return 0;
+    cin >> N;
+    cout << rec(N) << endl;
+    return 0;
 }
